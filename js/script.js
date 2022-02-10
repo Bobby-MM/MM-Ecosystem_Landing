5fe6ae33aa0d85e4402f17881c893cb99b9edd85
Bobby Spencer <bobby.spencer@maisonmoderne.com>
2022-02-10 10:25:28 +0100

    js logic on cards
    fixed.
    
     TODO : make it DRY

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 $(document).ready(function () {
     // When checkbox is checked display different text
-    $('#nl-selector').click(function() {
+
+    // Card 1
+    // TODO This needs to be cleaned and made DRY
+    $('#nl-selector1').click(function() {
         if ($(this).is(':checked')) {
             $(this).siblings('label').html('Retirer');
-            $(".card").addClass("added");
+            $(".card1").addClass("added");
         } else {
             $(this).siblings('label').html('Ajouter');
-            $(".card").removeClass("added");
+            $(".card1").removeClass("added");
+        }
+    });
+
+    $('#nl-selector2').click(function() {
+        if ($(this).is(':checked')) {
+            $(this).siblings('label').html('Retirer');
+            $(".card2").addClass("added");
+        } else {
+            $(this).siblings('label').html('Ajouter');
+            $(".card2").removeClass("added");
         }
     });
 });
+
