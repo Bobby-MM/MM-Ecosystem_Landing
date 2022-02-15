9ceced8c526265758c5ca0dce47b6cc19bea90a0
Bobby Spencer <bobby.spencer@maisonmoderne.com>
2022-02-15 17:57:24 +0100

    EOD
    end of day push

--- js/script.js
+++ js/script.js
@@ -39,6 +39,87 @@ $(document).ready(function () {
         }
     });
 
+    // Newsletter 3
+    $('#nl-selector3').click(function() {
+        if ($(this).is(':checked')) {
+            $(".nl-card3").addClass("added");
+        } else {
+            $(".nl-card3").removeClass("added");
+        }
+    });
+
+    // Newsletter 4
+    $('#nl-selector4').click(function() {
+        if ($(this).is(':checked')) {
+            $(".nl-card4").addClass("added");
+        } else {
+            $(".nl-card4").removeClass("added");
+        }
+    });
+
+    // Newsletter 5
+    $('#nl-selector5').click(function() {
+        if ($(this).is(':checked')) {
+            $(".nl-card5").addClass("added");
+        } else {
+            $(".nl-card5").removeClass("added");
+        }
+    });
+
+    // Newsletter 6
+    $('#nl-selector6').click(function() {
+        if ($(this).is(':checked')) {
+            $(".nl-card6").addClass("added");
+        } else {
+            $(".nl-card6").removeClass("added");
+        }
+    });
+
+    // Newsletter 7
+    $('#nl-selector7').click(function() {
+        if ($(this).is(':checked')) {
+            $(".nl-card7").addClass("added");
+        } else {
+            $(".nl-card7").removeClass("added");
+        }
+    });
+
+    // Newsletter 9
+    $('#nl-selector9').click(function() {
+        if ($(this).is(':checked')) {
+            $(".nl-card9").addClass("added");
+        } else {
+            $(".nl-card9").removeClass("added");
+        }
+    });
+
+    // Newsletter 10
+    $('#nl-selector9').click(function() {
+        if ($(this).is(':checked')) {
+            $(".nl-card10").addClass("added");
+        } else {
+            $(".nl-card10").removeClass("added");
+        }
+    });
+
+    // Newsletter 11
+    $('#nl-selector11').click(function() {
+        if ($(this).is(':checked')) {
+            $(".nl-card11").addClass("added");
+        } else {
+            $(".nl-card11").removeClass("added");
+        }
+    });
+
+    // Newsletter 12
+    $('#nl-selector12').click(function() {
+        if ($(this).is(':checked')) {
+            $(".nl-card12").addClass("added");
+        } else {
+            $(".nl-card12").removeClass("added");
+        }
+    });
+
     // * Hide fixed nav content on click
     $('.mm-form-top').click(function() {
         $(".hidden-content").toggleClass("navbar-hide");
